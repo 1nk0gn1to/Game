335d26266b50c9bd93ef8f9ad8d95c5e353caf39
1nk0gn1to <1nk0gn1to@mail.ru>
2023-05-30 22:13:36 +0500

    Add new script PlayerAnimator and rename old script Movement to PlayerMovement.

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+    [SerializeField] private Animator animator;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    void Update()
+    {
+        animator.SetBool(IS_WALKING, playerMovement.IsWalking());
+    }
+}
